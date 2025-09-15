7aa90b3dd82062bd55c323b3a261ff849cbb902e
atg99 <atg9910@naver.com>
2025-09-15 16:41:36 +0900

    Initial commit

---
+++ FileName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int FruitCount = -10;
+
+	string HelloWorld = "Hello World";
+
+	cout << HelloWorld << endl; cout << FruitCount << endl;
+
+	return 0;
+}
