9a45b43c1d2c563ae9195444bd6a967771f5e17f
atg99 <atg9910@naver.com>
2025-09-22 09:15:23 +0900

    0915

--- FileName.cpp
+++ FileName.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-	int FruitCount = -10;
+	int sum = 0;
+	int sum3 = 0;
+	for (int i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum3 += i;
+		}
+		sum += i;
+	}
 
-	string HelloWorld = "Hello";
-
-	cout << HelloWorld << endl; cout << FruitCount << endl;
-
+	cout << sum << endl;
+	cout << sum3 << endl;
 	return 0;
 }
