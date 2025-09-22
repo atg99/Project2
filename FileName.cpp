35d0f3c134afd8a12ce0450687ad28ef6b5565e5
atg99 <atg9910@naver.com>
2025-09-22 09:40:20 +0900

    최적화

--- FileName.cpp
+++ FileName.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 int main()
 {
-	int sum = 0;
-	int sum3 = 0;
-	for (int i = 1; i < 101; i++)
+	int Sum = 0;
+	int Sum3 = 0;
+	for (int i = 1; i < 101; ++i)
 	{
-		if (i % 3 == 0)
-		{
-			sum3 += i;
-		}
-		sum += i;
+		Sum += i;
 	}
 
-	cout << sum << endl;
-	cout << sum3 << endl;
+	for (int i = 3; i <= 100; i += 3)
+	{
+		Sum3 += i;
+	}
+	//cout 버퍼에 저장
+	//endl 버퍼 해제
+	cout << Sum << endl;
+	cout << Sum3 << endl;
 	return 0;
 }
